99be395e3e69e3d59848df4b1e5b39a24b916a86
Nikita Rogalenko <nikitalr@mail.ru>
2018-11-18 18:42:18 +0300

    free + some paste from book

--- RogoSPLLab6/mem_alloc.c
+++ RogoSPLLab6/mem_alloc.c
@@ -5,11 +5,17 @@
 #include <stdbool.h>
 #include "mem_alloc.h"
 
-void rogalloc() {
+void rogoFree(void* memChunk) {
+
+    struct mem* chunk = (struct mem*) ((char*) memChunk - sizeof(struct mem));
+    chunk->is_free = true;
+    while (chunk->next != NULL && chunk->next->is_free) {
+        chunk->capacity = chunk->capacity + chunk->next->capacity + sizeof(struct mem);
+        chunk->next = chunk->next->next;
+    }
 
 }
 
-void rogoFree() {
+void* rogalloc(size_t query) {
 
 }
-
--- RogoSPLLab6/mem_alloc.h
+++ RogoSPLLab6/mem_alloc.h
@@ -4,14 +4,37 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/mman.h>
 
 #ifndef ROGOSPLLAB6_MEM_ALLOC_H
 #define ROGOSPLLAB6_MEM_ALLOC_H
+
+#define HEAP_START ((void*)0x04040000)
+#define _USE_MISC
 
 struct mem {
     struct mem* next;
     size_t capacity;
     bool is_free;
 };
+
+/*#pragma pack(push, 1)
+struct mem {
+    struct mem* next;
+    size_t capacity;
+    bool is_free;
+};
+#pragma pack(pop)*/
+
+void* rogalloc( size_t query );
+void rogoFree( void* memChunk );
+void* heap_init( size_t initial_size );
+#define DEBUG_FIRST_BYTES 4
+void memalloc_debug_struct_info( FILE* f,
+                                 struct mem const* const address );
+void memalloc_debug_heap( FILE* f, struct mem const* ptr );
+
 
 #endif //ROGOSPLLAB6_MEM_ALLOC_H
