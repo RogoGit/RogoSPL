a2b1005b3572207305fede807f739808318afc2f
Nikita Rogalenko <nikitalr@mail.ru>
2019-06-09 14:56:29 +0300

    makefile and init process started

--- RogoForth/Forth_Kernel/RogoForthress.asm
+++ RogoForth/Forth_Kernel/RogoForthress.asm
@@ -11,25 +11,32 @@ section .text
 
 %include "forth_words.inc"    ;  words are here
 
-section .bss
-
-resq 1023              
-rstack_start: resq 1   
-
-user_mem: resq 65536 
+section .rodata
+not_found_msg: db ": no such word", 10, 0
 
 section .data
 
 lw: dq last_word       ; stores a pointer to the last word in dictionary
                        ; should be placed after all words are defined
 dp: dq user_mem        ; current global data pointer
+here: dq user_dict
 stack_start:  dq 0     ; stores a saved address of data stack
 forth_init: dq token_selector 
- 
+
+section .bss
+
+resq 1023              
+ret_stack_start: resq 1   
+
+input_buf: resb 1024   ; buffer to read textual words into
+user_dict:  resq 65536 ; data for words
+user_mem: resq 65536   ; global data for user
+state: resq 1          ; changes to 1 if compiling, 0 by default
+
 section .text
 
 _start: 
-    mov ret_stack, rstack_start
+    mov ret_stack, ret_stack_start
     mov [stack_start], rsp
     mov pc, forth_init
     jmp next
