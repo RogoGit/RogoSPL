5c9aeb2b826fdebfb70b3aa7621573df55042d0f
Nikita Rogalenko <nikitalr@mail.ru>
2019-11-16 11:58:58 +0300

    [SPF5-Part2] POSIX semaphores improvement

--- System_programming/SPFoundationsLab5/Part_2/sem_library_func.c
+++ System_programming/SPFoundationsLab5/Part_2/sem_library_func.c
@@ -6,39 +6,43 @@
 #include <unistd.h>
 
 sem_t semaphore;
-char letters_array[26] = "abcdefghijklmnopqrstuvwxyz";
+volatile char letters_array[26] = "abcdefghijklmnopqrstuvwxyz";
 
 void* change_case() {
-    unsigned short i = 0;
-    char ch;
-    sem_wait(&semaphore);
-    while (letters_array[i] != '\0') {
-        ch = letters_array[i];
-        if (ch >= 'A' && ch <= 'Z')
-            letters_array[i] = (char)(letters_array[i] + 32);
-        else if (ch >= 'a' && ch <= 'z')
-            letters_array[i] = (char)(letters_array[i] - 32);
-        i++;
+    while (1) {
+        sem_wait(&semaphore);
+        unsigned short i = 0;
+        char ch;
+        while (letters_array[i] != '\0') {
+            ch = letters_array[i];
+            if (ch >= 'A' && ch <= 'Z')
+                letters_array[i] = (char) (letters_array[i] + 32);
+            else if (ch >= 'a' && ch <= 'z')
+                letters_array[i] = (char) (letters_array[i] - 32);
+            i++;
+        }
+        sleep(1);
+        printf("Current array: %s\n", letters_array);
+        sem_post(&semaphore);
+        sleep(1);
     }
-    printf("Current array: %s\n",letters_array);
-    sleep(1);
-    sem_post(&semaphore);
-    return NULL;
 }
 
 void* reverse() {
-    unsigned short i = 0;
-    char temp_ch;
-    sem_wait(&semaphore);
-    for (i = 0; i < 13; i++){
-        temp_ch = letters_array[i];
-        letters_array[i] = letters_array[25 - i];
-        letters_array[25-i] = temp_ch;
+    while (1) {
+        sem_wait(&semaphore);
+        unsigned short i = 0;
+        char temp_ch;
+        for (i = 0; i < 13; i++) {
+            temp_ch = letters_array[i];
+            letters_array[i] = letters_array[25 - i];
+            letters_array[25 - i] = temp_ch;
+        }
+        sleep(1);
+        printf("Current array: %s\n", letters_array);
+        sem_post(&semaphore);
+        sleep(1);
     }
-    printf("Current array: %s\n",letters_array);
-    sleep(1);
-    sem_post(&semaphore);
-    return NULL;
 }
 
 int main () {
@@ -48,13 +52,12 @@ int main () {
 
     sem_init(&semaphore, 0, 1);
 
-    while (1) {
-        pthread_create(&thread1, NULL, change_case, NULL);
-        pthread_create(&thread2, NULL, reverse, NULL);
-        pthread_join(thread1, NULL);
-        pthread_join(thread2, NULL);
-    }
-    
+    pthread_create(&thread1, NULL, change_case, NULL);
+    pthread_create(&thread2, NULL, reverse, NULL);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
     sem_destroy(&semaphore);
 
     return 0;
