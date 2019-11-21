cbc772d55ca1eafcc6085ef963f4ac94e6c973d6
Nikita Rogalenko <nikitalr@mail.ru>
2019-11-21 23:36:52 +0300

    [SPF5-Part3] Signals on Perl

---
+++ System_programming/SPFoundationsLab5/Part_3/server_signals.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -T
+
+use strict;
+use warnings qw(FATAL all);
+use English;
+
+# variables
+my $pid = $$;
+my $uid = $<; 
+my @gr_list = split(/ /, $(); 
+my $gid = $gr_list[0];
+
+my $start_time = time();
+my $run_time;
+
+my @sys_load;
+
+my $signal;
+
+# functions
+sub update_info {
+	$run_time = time() - $start_time;
+	$ENV{'PATH'} = '/usr/bin';
+	@sys_load = split(/load average: /, qx(uptime));
+    @sys_load = split(', ', $sys_load[1]);
+}
+
+# main
+print("Server is running...\n");
+
+$SIG{HUP} = sub{ print "PID: $pid\n"; };
+$SIG{INT} = sub{ print "UID: $uid\n"; };
+$SIG{TERM} = sub{ print "GID: $gid\n"; };
+$SIG{USR1} = sub{ print "Server works: $run_time\n"; };
+$SIG{USR2} = sub{ print "Average load system time:\n \t1 minute - $sys_load[0];\n \t5 minutes - $sys_load[1];\n \t15 minutes - $sys_load[2]\n"; };
+
+while(1) {
+	update_info;
+	sleep(1);
+}
